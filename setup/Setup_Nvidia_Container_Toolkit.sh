6ef27b6d1545e3cdd33734293cd6dec77932f9c6
André Peuker <a.suennemann@edv-peuker.de>
2025-12-19 23:38:50 +0100

    Update Setup_Nvidia_Container_Toolkit.sh
    remove nvidia-open

--- setup/Setup_Nvidia_Container_Toolkit.sh
+++ setup/Setup_Nvidia_Container_Toolkit.sh
@@ -5,7 +5,7 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
 apt update
-apt install cuda-toolkit linux-headers-$(uname -r) nvidia-open cuda-drivers -y
+apt install cuda-toolkit linux-headers-$(uname -r) cuda-drivers -y
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g'     | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 apt update
